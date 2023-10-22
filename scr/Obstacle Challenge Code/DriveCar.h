136e2d64efe3a29f2bf2feefbcd1ec7f9513dd50
wroqabatyacaesar2023 <145048454+wroqabatyacaesar2023@users.noreply.github.com>
2023-10-22 22:04:39 +0300

    Update DriveCar.h

--- scr/Obstacle Challenge Code/DriveCar.h
+++ scr/Obstacle Challenge Code/DriveCar.h
@@ -5,13 +5,14 @@
 #define UNKNOWN_DIRECTION -1
 
 #define STRAIGHT_STEERING 88
-//#define STRAIGHT_STEERING 90
+// #define STRAIGHT_STEERING 90
 #define SPEED2 200
 #define SLOW_SPEED2 175
+#define SLOW_SPEED3 165
 #define SLOW_SPEED 150
-//int initR,initL,initF;
-int rounCounter=0;
-int rotionDirection=UNKNOWN_DIRECTION;
+// int initR,initL,initF;
+int rounCounter = 0;
+int rotionDirection = UNKNOWN_DIRECTION;
 bool pinAttached;
 Servo steering;
 int steeringAngle;
@@ -20,66 +21,101 @@ int steeringAngle;
 const int freq = 40000;
 const int pwmChannel = 2;
 const int resolution = 8;
-//int dutyCycle = 200;
+// int dutyCycle = 200;
 volatile unsigned long encoder_count = 0;
 int speed;
 unsigned long driveTime = 0;
 
- void IRAM_ATTR isr() {
-	encoder_count++;
+void IRAM_ATTR isr()
+{
+  encoder_count++;
 }
-void initCar() {
+void initCar()
+{
 
-  speed=0;
+  speed = 0;
   steering.attach(STEERING_PIN);
-  //steeringAngle=89; //  >90 Rigth     <90 LEFT
+  // steeringAngle=89; //  >90 Rigth     <90 LEFT
   steering.write(STRAIGHT_STEERING);
   // put your setup code here, to run once:
- pinMode(PWM,OUTPUT);
- pinMode(IN1,OUTPUT);
- pinMode(IN2,OUTPUT);
- pinMode(ENCODER_PIN,INPUT);
+  pinMode(PWM, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENCODER_PIN, INPUT);
 
-attachInterrupt(ENCODER_PIN, isr, CHANGE);
-   // configure LED PWM functionalitites
- ledcSetup(pwmChannel, freq, resolution);
-  
+  attachInterrupt(ENCODER_PIN, isr, CHANGE);
+  // configure LED PWM functionalitites
+  ledcSetup(pwmChannel, freq, resolution);
+
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(PWM, pwmChannel);
-  pinAttached=true;
- digitalWrite(IN1,LOW);
- digitalWrite(IN2,LOW);
- speed=0;
+  pinAttached = true;
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  speed = 0;
 
-  steering.setPeriodHertz(50);// standard 50 hz servo
-	//myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
-
+  steering.setPeriodHertz(50); // standard 50 hz servo
+                               // myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
 }
-void setSteering(int ang){
-   steering.write(ang);
-}
-
-void forward(int speed){
-  //ledcAttachPin(PWM, pwmChannel);
-  //analogWrite(PWM,speed);
-  if(speed>254){if(pinAttached){ledcDetachPin(PWM); pinAttached=false;} digitalWrite(PWM,HIGH);}
-  else{if(!pinAttached){ledcAttachPin(PWM, pwmChannel); pinAttached=true;}ledcWrite(pwmChannel, speed); } 
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-
-}
-void reverse(int speed){
-  Serial.println("Drive Revirce");
-  //analogWrite(PWM,speed);
-  if(speed>254){if(pinAttached){ledcDetachPin(PWM); pinAttached=false;} digitalWrite(PWM,HIGH);}
-  else{if(!pinAttached){ledcAttachPin(PWM, pwmChannel); pinAttached=true;}ledcWrite(pwmChannel, speed); } 
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
+void setSteering(int ang)
+{
+  steering.write(ang);
 }
 
-
-
-void stop(){
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,LOW);
+void forward(int speed)
+{
+  // ledcAttachPin(PWM, pwmChannel);
+  // analogWrite(PWM,speed);
+  if (speed > 254)
+  {
+    if (pinAttached)
+    {
+      ledcDetachPin(PWM);
+      pinAttached = false;
+    }
+    digitalWrite(PWM, HIGH);
+  }
+  else
+  {
+    if (!pinAttached)
+    {
+      ledcAttachPin(PWM, pwmChannel);
+      pinAttached = true;
+    }
+    ledcWrite(pwmChannel, speed);
+  }
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
 }
+void reverse(int speed)
+{
+  // Serial.println("Drive Revirce");
+  // analogWrite(PWM,speed);
+  if (speed > 254)
+  {
+    if (pinAttached)
+    {
+      ledcDetachPin(PWM);
+      pinAttached = false;
+    }
+    digitalWrite(PWM, HIGH);
+  }
+  else
+  {
+    if (!pinAttached)
+    {
+      ledcAttachPin(PWM, pwmChannel);
+      pinAttached = true;
+    }
+    ledcWrite(pwmChannel, speed);
+  }
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+}
+
+void stop()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
+
