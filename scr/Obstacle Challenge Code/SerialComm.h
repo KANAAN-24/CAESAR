4d64c78795a5cae2d351b64069cd655b70fb168b
KANAAN <145048454+wroqabatyacaesar2023@users.noreply.github.com>
2024-08-28 13:09:18 +0300

    V-1

--- scr/Obstacle Challenge Code/SerialComm.h
+++ scr/Obstacle Challenge Code/SerialComm.h
@@ -4,18 +4,20 @@ char c;
 String msg;
 void initSerial() {
 
-  Serial.begin(115200);
+ Serial.begin(115200);
+
 }
 
 void readSerial() {
-  msg = "";
-  if (Serial.available()) {
-    msg = "";
-    while (Serial.available()) {
-      c = Serial.read();
-      msg = msg + c;
-    }
-    Serial.print("msg is:");
-    Serial.println(msg);
+  msg="";
+  if (Serial.available())  {
+    msg="";
+      while(Serial.available()){
+    c=Serial.read();
+    msg=msg+c;
+  
+  } 
+  Serial.print("msg is:"); Serial.println(msg); 
   }
+
 }
