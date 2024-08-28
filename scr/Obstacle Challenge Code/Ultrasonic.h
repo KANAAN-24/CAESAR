6b8d32a1966fc5c64238aa10a8fd92db3608b4fe
KANAAN <145048454+wroqabatyacaesar2023@users.noreply.github.com>
2024-08-28 13:10:08 +0300

    V-1

--- scr/Obstacle Challenge Code/Ultrasonic.h
+++ scr/Obstacle Challenge Code/Ultrasonic.h
@@ -5,113 +5,144 @@
 
 
 #define TIMEOUT 16500
-HardwareSerial ultraSensor(1);
-unsigned int rangeR, rangeL, rangeF;
-unsigned int initR, initL, initF, rangeB;
-unsigned int prevRangeR, prevRangeL, prevRangeF;
+HardwareSerial ultraSensor(1); 
+unsigned int rangeR,rangeL,rangeF;
+unsigned int initR,initL,initF,rangeB;
+unsigned int prevRangeR,prevRangeL,prevRangeF;
 
 unsigned long UltraSonicUpdateTime;
 float distance;
 unsigned long period;
-void initUltrasonic() {
-  pinMode(ECHO_R, INPUT);
-  pinMode(TRIG_R, OUTPUT);
-  pinMode(ECHO_L, INPUT);
-  pinMode(TRIG_L, OUTPUT);
-  pinMode(ECHO_F, INPUT);
-  pinMode(TRIG_F, OUTPUT);
-  pinMode(ECHO_B, INPUT);
-  pinMode(TRIG_B, OUTPUT);
+void initUltrasonic(){
+  
+  
+pinMode(ECHO_R,INPUT);
+pinMode(TRIG_R,OUTPUT);
+pinMode(ECHO_L,INPUT);
+pinMode(TRIG_L,OUTPUT);
+pinMode(ECHO_F,INPUT);
+pinMode(TRIG_F,OUTPUT);
+pinMode(ECHO_B,INPUT);
+pinMode(TRIG_B,OUTPUT);
+
+
 }
 
-int findeDist(byte E, byte T) {
-  int d;
-  digitalWrite(T, LOW);
-  delayMicroseconds(5);
-  digitalWrite(T, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(T, LOW);
-  period = pulseIn(E, HIGH, 17492);
-  distance = float(period) / 58.8;
-  d = (int)distance;
-  d = constrain(d, 0, 301);
-  if (d == 0) { d = 300; }
-  return (d);
+int findeDist(byte E,byte T){
+int d;
+digitalWrite(T,LOW);
+delayMicroseconds(5);
+digitalWrite(T,HIGH);
+delayMicroseconds(10);
+digitalWrite(T,LOW);
+period=pulseIn(E,HIGH,17492);
+distance=float(period)/58.8;
+d=(int)distance; 
+d=constrain(d, 0, 301);
+if(d==0){d=300;}
+return(d);
 }
 
-void readUltrasonic() {
-  prevRangeR = rangeR;
-  prevRangeL = rangeL;
-  prevRangeF = rangeF;
-  rangeR = findeDist(ECHO_R, TRIG_R);
-  rangeF = findeDist(ECHO_F, TRIG_F);
-  rangeL = findeDist(ECHO_L, TRIG_L);
-  // rangeF=findeDist(ECHO_F,TRIG_F);
+void readUltrasonic()
+{
+  prevRangeR=rangeR;
+  prevRangeL=rangeL;
+  prevRangeF=rangeF;
+  rangeR=findeDist(ECHO_R,TRIG_R);
+  rangeF=findeDist(ECHO_F,TRIG_F);
+  delay(3);
+  rangeL=findeDist(ECHO_L,TRIG_L);
+  delay(2);
+ // rangeF=findeDist(ECHO_F,TRIG_F);
+  
 }
 
-unsigned int readForwardDistance(int times) {
-  unsigned int dd = 0;
-  for (int i = 0; i < times; i++) {
-    dd = dd + findeDist(ECHO_F, TRIG_F);
-    delay(1);
+unsigned int readForwardDistance (int times){
+  unsigned int dd=0;
+  for(int i=0;i<times;i++){
+  dd=dd+findeDist(ECHO_F,TRIG_F);delay(1);
   }
-  rangeF = dd / times;
-  return rangeF;
-}
-
-unsigned int readBackDistance(int times) {
-  unsigned int dd = 0;
-  for (int i = 0; i < times; i++) {
-    dd = dd + findeDist(ECHO_B, TRIG_B);
-    delay(1);
+  rangeF=dd/times;
+   return rangeF;
   }
-  rangeB = dd / times;
-  return rangeB;
-}
 
-void readDistance(int readCase) {
-  prevRangeR = rangeR;
-  prevRangeL = rangeL;
-  prevRangeF = rangeF;
+unsigned int readBackDistance(int times){
+  unsigned int dd=0;
+  for(int i=0;i<times;i++){
+   delay(1); dd=dd+findeDist(ECHO_B,TRIG_B);delay(1);
+  }
+  rangeB=dd/times;
+   return rangeB;
+  }
 
-  switch (readCase) {
-    case ALL:
-      rangeR = findeDist(ECHO_R, TRIG_R);
-      rangeF = findeDist(ECHO_F, TRIG_F);
-      rangeL = findeDist(ECHO_L, TRIG_L);
+void readDistance(int readCase){
+  prevRangeR=rangeR;
+  prevRangeL=rangeL;
+  prevRangeF=rangeF;
+
+  switch (readCase){
+    case  ALL:
+      rangeR=findeDist(ECHO_R,TRIG_R);
+      rangeF=findeDist(ECHO_F,TRIG_F);
+      rangeL=findeDist(ECHO_L,TRIG_L);
       delay(1);
-      break;
-    case LEFT_RIGHT:
-      rangeR = findeDist(ECHO_R, TRIG_R);
-      delay(2);
-      rangeL = findeDist(ECHO_L, TRIG_L);
-      delay(2);
-
-      break;
+    break; 
+    case LEFT_RIGHT :
+       delay(5);
+          rangeL=findeDist(ECHO_L,TRIG_L);
+     
+      delay(5);
+          rangeR=findeDist(ECHO_R,TRIG_R);
+          
+      delay(5);
+      
+    break; 
     case LIFT_FORWARD:
-      rangeF = findeDist(ECHO_F, TRIG_F);
+      rangeF=findeDist(ECHO_F,TRIG_F);
       delay(2);
-      rangeL = findeDist(ECHO_L, TRIG_L);
+      rangeL=findeDist(ECHO_L,TRIG_L);
       delay(2);
-      break;
+    break;
     case RIGHT_FORWARD:
-      rangeR = findeDist(ECHO_R, TRIG_R);
+      rangeR=findeDist(ECHO_R,TRIG_R);
       delay(2);
-      rangeF = findeDist(ECHO_F, TRIG_F);
+      rangeF=findeDist(ECHO_F,TRIG_F);
       delay(2);
-      break;
+    break; 
+    }
+  
   }
-}
 
-void debugUltrasonic() {
-  Serial.print("R: ");
-  Serial.print(rangeR);
-  Serial.print("  L: ");
-  Serial.print(rangeL);
-  Serial.print("  F: ");
-  Serial.print(rangeF);
-  Serial.println();
-}
+void debugUltrasonicFrom(int readCase){
+           Serial.println("Read From : ");
+    switch (readCase){
+    case  ALL:
+             Serial.println("ALL");
+    break; 
+    case LEFT_RIGHT :
+        Serial.println("LEFT_RIGHT");
+    break; 
+    case LIFT_FORWARD:
+         Serial.println("LIFT_FORWARD");
+    break;
+    case RIGHT_FORWARD:
+           Serial.println("RIGHT_FORWARD");
+    break; 
+    }
+  
+  Serial.print("R: ");Serial.print(rangeR);
+  Serial.print("  L: ");Serial.print(rangeL);
+  Serial.print("  F: ");Serial.print(rangeF);
+  Serial.println();}
+
+
+  void debugUltrasonic(){
+   
+  
+  Serial.print("R: ");Serial.print(rangeR);
+  Serial.print("  L: ");Serial.print(rangeL);
+  Serial.print("  F: ");Serial.print(rangeF);
+  Serial.println();}
 
 /*
 char c;
