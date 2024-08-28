cc2b9ee65be6f08226751a836f7135718bba1862
KANAAN <145048454+wroqabatyacaesar2023@users.noreply.github.com>
2024-08-28 13:06:15 +0300

    V-1

--- scr/Obstacle Challenge Code/Rotation.h
+++ scr/Obstacle Challenge Code/Rotation.h
@@ -3,26 +3,23 @@
 
 MPU6050 mpu(Wire);
 unsigned long mpu_timer = 0;
-int initAngle, currentAngle, angle0;
-void init_mpu() {
+int initAngle,currentAngle,angle0;
+void init_mpu(){   
   Wire.begin();
-
+  
   byte status = mpu.begin();
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);
-  while (status != 0) { ESP.restart(); }  // stop everything if could not connect to MPU6050
-
+  while(status!=0){ ESP.restart();} // stop everything if could not connect to MPU6050
+  
   Serial.println(F("Calculating offsets, do not move MPU6050"));
   delay(1000);
   // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
-  mpu.calcOffsets();  // gyro and accelero
+  mpu.calcOffsets(); // gyro and accelero
   Serial.println("Done!\n");
 }
-void readAngle() {
+void readAngle(){
   mpu.update();
-  currentAngle = (int)mpu.getAngleZ();
-}
-void debugRotation() {
-  Serial.print("Angle= ");
-  Serial.println(currentAngle);
-}
+  currentAngle=(int)mpu.getAngleZ();
+  }
+void debugRotation(){Serial.print("Angle= "); Serial.println(currentAngle);}
