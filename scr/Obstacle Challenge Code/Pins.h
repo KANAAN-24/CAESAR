c60723dfe6e8eeb824a63730396e10b060e3e056
KANAAN <145048454+wroqabatyacaesar2023@users.noreply.github.com>
2024-08-28 13:03:18 +0300

    V-1

--- scr/Obstacle Challenge Code/Pins.h
+++ scr/Obstacle Challenge Code/Pins.h
@@ -2,7 +2,7 @@
 #define Pins_h
 //Start and mode
 #define START_BUTTON 39
-#define MODE_KEY 36  // level1 or level2
+#define MODE_KEY 36 // level1 or level2
 //LED and colors
 #define LED_RED 23
 #define LED_BLUE 4
@@ -33,7 +33,7 @@
 #define TRIG_R 18
 #define ECHO_L 16
 #define TRIG_L 17
-#define ECHO_F 2
+#define ECHO_F 2       
 #define TRIG_F 15
 
 //#define LED_POWER 1
